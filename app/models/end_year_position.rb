ca62c56f9eaef0199cbadaa659f2660223aeeb35
Nilson de Souza Mira Júnior <mira.sni@gmail.com>
2022-08-15 17:00:17 -0300

    Round money values on list and show

--- app/models/end_year_position.rb
+++ app/models/end_year_position.rb
@@ -1,3 +1,17 @@
 class EndYearPosition < ApplicationRecord
   belongs_to :asset
+
+  rails_admin do
+    configure :average_price do
+      pretty_value do
+        value.round(2)
+      end
+    end
+
+    configure :total_cost do
+      pretty_value do
+        value.round(2)
+      end
+    end
+  end
 end
